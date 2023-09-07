ab59a1219b552f4ec2047dde481a8ede51ba259f
DoanQuynhChi3112 <doanthiquynhchi3112@gmail.com>
2023-09-07 17:15:03 +0700

    Toan tu

--- newApp/Program.cs
+++ newApp/Program.cs
@@ -1,14 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("2021050114");
-int a = 9;
-Console.WriteLine("a =" + a);
-
-    const string hoten = "Doan Thi Quynh Chi";
-    const int msv = 2021050114;
-    Console.WriteLine(" Sinh Vien " + hoten + "-" + "MSV" + msv);
-
+﻿using System;
+using System.IO.Compression;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("DoanThiQuynhChi - 2021050114");
+        int a,b;
+        System.Console.Write(" a = ");
+        a = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write(" b = ");
+        b = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
     
+    }
 
-
- 
+}

02c3097c25ec6bb5541824643c0bac60e416fef0
DoanQuynhChi3112 <doanthiquynhchi3112@gmail.com>
2023-09-07 17:01:11 +0700

    khai bao

--- newApp/Program.cs
+++ newApp/Program.cs
@@ -3,3 +3,12 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("2021050114");
 int a = 9;
 Console.WriteLine("a =" + a);
+
+    const string hoten = "Doan Thi Quynh Chi";
+    const int msv = 2021050114;
+    Console.WriteLine(" Sinh Vien " + hoten + "-" + "MSV" + msv);
+
+    
+
+
+ 

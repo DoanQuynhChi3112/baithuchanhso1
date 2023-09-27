67c7600e0f1bff4416b11a6c58d67e1f56eeabee
DoanQuynhChi3112 <doanthiquynhchi3112@gmail.com>
2023-09-27 22:41:15 +0700

    khoi tao class

---
+++ newApp/Models/Employee.cs
@@ -0,0 +1,24 @@
+namespace NewApp.Models
+ {
+ public class Employee {
+        public string FullName {get;set;}
+        public string Address {get; set;}
+        public string EmployeeID {get; set;}
+
+        // phuong thuc co gia tri tra ve-Employee
+        public Employee()
+        {
+            FullName = "Ho Ten ";
+            Address ="Dia chi";
+            EmployeeID ="Ma nhan vien";
+        }
+        public int TinhLuong(int LuongCB)
+            {
+                
+                int Luong = LuongCB + 120000;
+                return Luong;
+            }
+         
+ }
+ } 
+        
---
+++ newApp/Models/Student.cs
@@ -0,0 +1,34 @@
+ namespace NewApp.Models
+ {
+ public class Students  {
+        public string Name {get;set;}
+        public string Address {get; set;}
+        public string StudentID {get; set;}
+        
+        
+   
+    // phuong co gia tri tra ve-student
+        public Students ()
+        {
+            Name = "ho ten mac dinh";
+            Address =" Phu Tho";
+            StudentID ="2021050114";
+        }
+        public int GetYearOfBirth(int age)
+    {
+        int yearOfBirth = 2023-age;
+        return yearOfBirth;
+    }
+
+        // pulic void Display()
+        //     {
+                
+        //         System.Console.WriteLine("{0}-{1}-{2} ",Name, Address, StudentID);
+        //     }
+        //  public void Display2(string ten, int tuoi)
+        //     {
+                
+        //         System.Console.WriteLine("{0}-{1} ",ten, tuoi);
+        //     }
+ }
+ } 

7e09fabe91257d0cc957a9d93143377360440f9e
DoanQuynhChi3112 <doanthiquynhchi3112@gmail.com>
2023-11-01 23:16:07 +0700

    Slide 17(2)

---
+++ Firtmvc/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Firtmvc.Controllers;
+
+public class PersonController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(string fname, string ID)
+    {
+        string strResult = "Welcome : " + fname.ToUpper() + " - " + ID;
+        ViewBag.Nhapthongtin = strResult;
+        return View();
+    }
+
+}
